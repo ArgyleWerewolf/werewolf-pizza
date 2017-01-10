ddc58f82b748a6f209d7f9b4f4dda071ac77ba11
A. Quinton <argylewerewolf@gmail.com>
2017-01-10 06:36:08 -0800

    toggle statelogger visibility for cleaner layout; statelogger is partially transparent when visible

--- src/components/StateLogger/StateLogger.jsx
+++ src/components/StateLogger/StateLogger.jsx
@@ -3,10 +3,33 @@ import React from 'react';
 require('./StateLogger.scss');
 
 export default class StateLogger extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  toggle () {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
   render () {
-    return (
-      <pre className="statelogger">{JSON.stringify(this.props.state, null, 2)}</pre>
-    );
+
+    let logger;
+
+    if (this.state.open) {
+      logger = (
+        <pre className="statelogger statelogger-state" onClick={this.toggle.bind(this)}>{JSON.stringify(this.props.state, null, 2)}</pre>
+      );
+    } else {
+      logger = <button className="statelogger statelogger-button" onClick={this.toggle.bind(this)}>Show State</button>;
+    }
+
+    return logger;
   }
 }
 
